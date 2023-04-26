ecf4816a085437b30832bc29e53bf23d9f0dab16
CursedSheep <tyronederp16@gmail.com>
2023-04-26 22:56:06 +0800

    Revert "Delete BotConfiguration.cs"
    This reverts commit 407d2b7902fd52c695aee8295b0d2873e5ed88b8.

---
+++ Connect4_house/BotConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4_house
+{
+    internal class BotConfiguration
+    {
+        public static string BotName = "Connect4";
+        public static string Token = "MTEwMDM4ODQxOTE4NTY4ODYzNg.G0H2_U.qeQFr2e5bhP5-w0egZeDUS6Lq5r0ueH0dBEA7g";
+    }
+}
